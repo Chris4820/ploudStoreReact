a20c646d34f13c27e4a58f21f232dbfad8279cb4
Pong-Code <chrismoreiraa02@gmail.com>
2024-05-22 16:18:55 +0100

    AAAA

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    }
   }
 })
