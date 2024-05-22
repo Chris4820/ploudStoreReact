608755f62782b0745f222975c17e15983a95896c
Pong-Code <chrismoreiraa02@gmail.com>
2024-05-22 17:30:29 +0100

    Req

--- src/lib/axios/axiosAuth.ts
+++ src/lib/axios/axiosAuth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_URL || 'http://localhost:3000/api/auth'; // Default fallback
+const baseURL = import.meta.env.VITE_URL + "/api/auth" || 'http://localhost:3000/api/auth'; // Default fallback
 
 console.log('VITE_URL:', import.meta.env.VITE_URL); // Deve mostrar o valor da variável de ambiente
 console.log('Base URL:', baseURL); // Deve mostrar a URL base final usada pelo axios
