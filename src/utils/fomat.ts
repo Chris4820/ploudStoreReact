e92e5a3fff4a736878bf4ef54bbda47b3a17e0c0
Chris <chrismoreiraa02@gmail.com>
2025-02-17 15:54:04 +0000

    Remove asycn

--- src/utils/fomat.ts
+++ src/utils/fomat.ts
@@ -5,7 +5,7 @@ import { type StoreInformationProps } from "../features/stores/api/req/store";
 
 
 
-export async function formatMoney(price: number, store: StoreInformationProps | undefined) {
+export function formatMoney(price: number, store: StoreInformationProps | undefined) {
   const finalPrice = (price / 100);
   if(store && store.locale && store.currency) {
     const formated = Intl.NumberFormat(store.locale, {
